383d9a0a4668a50ecb184427c9ae653d09060b5c
Daniel Espitia <xespitiacorredo@stuba.sk>
2024-10-02 16:11:44 +0200

    main Page development in progress
    - Basic skeleton of the main internal page of the application

---
+++ quasar-vpwa/src/components/ChatTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="chat-parent q-col q-col-12 full-width" style="overflow-y: auto;">
+        <!-- Render chat messages -->
+<!--
+    We should connect the message and properties from the database,
+    when user do log In.
+-->
+     <div v-for="(message, index) in messages" :key="index";>
+        <!--Message sent by another user-->
+        <q-chat-message
+            :text="[message.text]"
+            :name="message.sender"     
+            :reverse="message.sender === userNickname"
+            :sent="message.sender === userNickname"
+            :received="message.sender !== userNickname"
+        /> <!--:avatar="message.avatar"-->
+         </div>
+
+    </div>
+</template>
+<script>
+
+
+</script>
---
+++ quasar-vpwa/src/pages/mainPageApp.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="chat-page q-gutter-sm">
+        <!--Sidebar-->
+        <div class="side-box q-pa-xs">
+            <SidebarComponent />
+        </div>
+        <!--ChannelsBox-->
+        <div class="channels-box q-pa-xs">
+            <ChannelsComponent />
+        </div>
+        <!--ChatBox-->
+        <div class="chat-box q-pa-xs">
+            <ChatComponent />
+        </div>
+    </div>
+</template>    
+
+<script setup>
+import SidebarComponent from './SidebarComponent.vue';
+import ChannelsComponent from './ChannelsComponent.vue';
+import ChatComponent from './ChatComponent.vue';
+</script>
+
+<style lang="scss" scoped>
+@import 'src/css/quasar.variables.scss';
+
+.chat-page {
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+}
+.side-box {
+    width: 80px;
+    color: white;
+  }
+.channels-box {
+    flex: 3; 
+  }
+.chat-box{
+    flex: 1;
+}  
+
+  
+
+</style>
