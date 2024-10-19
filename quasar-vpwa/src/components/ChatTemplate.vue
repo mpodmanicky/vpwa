eec26c15877dfb623a89cb9e5bc4de3698ea6c75
Martin Podmanicky <mpodmanicky34@gmail.com>
2024-10-19 23:10:42 +0200

    fixed-infinite-scroll-on-messages

--- quasar-vpwa/src/components/ChatTemplate.vue
+++ quasar-vpwa/src/components/ChatTemplate.vue
@@ -6,35 +6,59 @@
     <div class="statusOptions"></div>
   </div>
   <div class="messages">
-    <q-infinite-scroll @load="onLoad" reverse>
-      <p v-if="messages.length === 0">No messages yet.</p>
+  <q-infinite-scroll @load="onLoad" reverse>
+    <p v-if="messages.length === 0">No messages yet.</p>
       <MessageTemplate v-for="(msg, index) in messages" :key="index" :username="msg.username" :message="msg.text" />
+      <!-- Dummy div used for scrolling to the newest message -->
+      <div ref="newestMessage"></div>
     </q-infinite-scroll>
+  </div>
     <!--incoming messages will be displayed on the left side of the chat while outgoing on the right side-->
     <!--div needs to be scrollable-->
-  </div>
   <div class="commandline">
     <CommandlineTemplate @sendCommand="handleCommand" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, nextTick } from 'vue';
 import MessageTemplate from 'src/components/MessageTemplate.vue';
 import CommandlineTemplate from 'src/components/CommandlineTemplate.vue';
 
-const messages = ref([]);
+const messages = ref([]); // Holds the messages to display
+const allMessages = ref([ /* Full array of messages loaded from server */]); // This would be your server-side or full data
+const perPage = 5; // Number of messages to load per scroll
+const newestMessage = ref(null); // Reference for the newest message div
+
+// Loads more messages when scrolling up
+function onLoad() {
+  const currentLength = messages.value.length;
+
+  // Check if there are more messages to load
+  if (currentLength < allMessages.value.length) {
+    // Append more messages from allMessages array
+    const moreMessages = allMessages.value.slice(currentLength, currentLength + perPage);
+    messages.value.unshift(...moreMessages);
+  }
+
+  // Call done to tell Quasar infinite scroll component to stop loading
+  // done();
+};
+
+// Scrolls to the newest message after adding a new one
+function scrollToNewestMessage() {
+  nextTick(() => {
+    if (newestMessage.value) {
+      newestMessage.value.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+};
 
 function handleCommand(command) {
-  messages.value.push({ username: 'User', text: command})
+  messages.value.push({ username: 'User', text: command })
+  allMessages.value.push({ username: 'User', text: command });
 }
 
-function onLoad(index, done) {
-  setTimeout(() => {
-    messages.value.splice();
-    done();
-  }, 2000);
-}
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +71,14 @@ function onLoad(index, done) {
 #channel-name{
   padding: 5px;
   margin-left: 5px;
+}
+
+.messages {
+  height: calc(100vh - 250px);
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column-reverse;
+  flex-grow: 1;
 }
 
 .commandline {
